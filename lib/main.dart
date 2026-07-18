7ae009883913b12d290520e92738488d0f7b8486
Tevero Unokan <tevero96@gmail.com>
2026-07-18 21:52:33 +0100

    ReplyMate AI Phase 5 - Firebase Cloud Foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 import 'screens/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ReplyMateApp());
 }
 
@@ -18,4 +27,3 @@ class ReplyMateApp extends StatelessWidget {
     );
   }
 }
-
